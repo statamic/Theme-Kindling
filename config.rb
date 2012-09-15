d5a97c25425e6648921942c4cfa19d464694c6be
Jack McDade <jack@jackmcdade.com>
2012-09-14 23:56:37 -0400

    adds Compass config

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Compass configuration
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+environment = :production
+output_style = :compressed
+line_comments = false
+relative_assets = true
